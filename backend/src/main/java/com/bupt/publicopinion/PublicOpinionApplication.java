cb546f721f7f2314ee9f45503433a6d423e7de79
Candymoon <candymoon0627@gmail.com>
2026-07-07 16:40:21 +0800

    feat: add intelligence service config and exception handler

--- backend/src/main/java/com/bupt/publicopinion/PublicOpinionApplication.java
+++ backend/src/main/java/com/bupt/publicopinion/PublicOpinionApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan({"com.bupt.publicopinion.collection.mapper", "com.bupt.publicopinion.system.mapper", "com.bupt.publicopinion.content.mapper"})
+@MapperScan({"com.bupt.publicopinion.collection.mapper", "com.bupt.publicopinion.system.mapper", "com.bupt.publicopinion.content.mapper", "com.bupt.publicopinion.analysis.mapper"})
 public class PublicOpinionApplication {
 
     public static void main(String[] args) {
